f6cc0bc2fd708b9189b11de6804601f78d7d5a7b
Brandon-Magofna <magofna68@gmail.com>
2021-05-20 14:42:39 -0700

    build out restaurant controller & build out restaurant create view
    Co-authored-by: Brent Hubbard<hubbardbrent@hotmail.com>

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
+
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
