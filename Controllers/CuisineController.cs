6ebc81ee6ffaf93712c6ca2993beb79f139d9161
Brandon-Magofna <magofna68@gmail.com>
2021-05-20 14:39:46 -0700

    add a create/delete/details/edit view pages & built out home index view
    Co-authored-by: Brent Hubbard<hubbardbrent@hotmail.com>

--- Controllers/CuisineController.cs
+++ Controllers/CuisineController.cs
@@ -1,21 +1,46 @@
-public ActionResult Create()
-{
-  return View();
-}
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-[HttpPost]
-public ActionResult Create(Cuisine cuisine)
+namespace BestRestaurant.Controllers
 {
-  _db.Cuisine.Add(cuisine);
-  _db.SaveChanges();
-  return RedirectToAction("Index");
-}
+  public class CuisineController : Controller
+  {
+    private readonly BestRestaurantContext _db;
 
- public ActionResult Delete(int id)
+    public CuisineController(BestRestaurantContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Cuisine> model = _db.Cuisine.ToList();
+      return View(model);
+    }
+
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Cuisine cuisine)
+    {
+      _db.Cuisine.Add(cuisine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
     {
       var thisCuisine = _db.Cuisine.FirstOrDefault(restaurant => restaurant.CuisineId == id);
       return View(thisCuisine);
     }
+
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
@@ -24,12 +49,13 @@ public ActionResult Create(Cuisine cuisine)
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-public ActionResult Details(int id)
-{
-  Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
-  return View(thisCuisine);
-}
-public ActionResult Edit(int id)
+    public ActionResult Details(int id)
+    {
+      Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+      return View(thisCuisine);
+    }
+
+    public ActionResult Edit(int id)
     {
       var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
       return View(thisCuisine);
@@ -42,3 +68,9 @@ public ActionResult Edit(int id)
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+  }
+
+};
+
+
