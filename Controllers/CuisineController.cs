729d66829caf83f662b8793f420a05dfd6e0ddfa
Brandon-Magofna <magofna68@gmail.com>
2021-05-20 13:55:15 -0700

    add a delete actionresult to cuisine controller
    Co-authored-by: Brent Hubbard<hubbardbrent@hotmail.com>

--- Controllers/CuisineController.cs
+++ Controllers/CuisineController.cs
@@ -10,3 +10,17 @@ public ActionResult Create(Cuisine cuisine)
   _db.SaveChanges();
   return RedirectToAction("Index");
 }
+
+ public ActionResult Delete(int id)
+    {
+      var thisCuisine = _db.Cuisine.FirstOrDefault(restaurant => restaurant.CuisineId == id);
+      return View(thisCuisine);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisCuisine = _db.Restaurant.FirstOrDefault(cuisine => cuisine.CuisineId == id);
+      _db.Cuisine.Remove(thisCuisine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
